76669cd5a183762d4fabb8beef9a22e565b36ef7
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-01-31 01:05:52 +0530

    UrgencyPricingStrategy service added

---
+++ src/main/java/com/soumyajit/HotelBooking/Strategy/UrgencyPricingStrategy.java
@@ -0,0 +1,29 @@
+package com.soumyajit.HotelBooking.Strategy;
+
+import com.soumyajit.HotelBooking.entities.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class UrgencyPricingStrategy implements PricingStrategy {
+    private final PricingStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price = wrapped.calculatePrice(inventory);
+        LocalDate today = LocalDate.now();
+        LocalDate inventoryDate = inventory.getDate();
+        if (!inventoryDate.isBefore(today) && inventoryDate.isBefore(today.plusDays(7))){
+            price = price.multiply(BigDecimal.valueOf(1.15));
+        }
+        return price;
+    }
+}
+
+
+
