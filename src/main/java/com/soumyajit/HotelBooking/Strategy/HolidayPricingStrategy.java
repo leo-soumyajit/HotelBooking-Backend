dd86a8ea509479104167b17aa72e32a43bc87120
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-01-31 01:11:47 +0530

    HolidayPricingStrategy service is added

---
+++ src/main/java/com/soumyajit/HotelBooking/Strategy/HolidayPricingStrategy.java
@@ -0,0 +1,24 @@
+package com.soumyajit.HotelBooking.Strategy;
+
+import com.soumyajit.HotelBooking.entities.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class HolidayPricingStrategy implements PricingStrategy{
+
+    private final PricingStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price = wrapped.calculatePrice(inventory);
+        boolean isTodayHoliday = true ; //TODO : Create a Constant array or ArrayList to store Holidays
+        if (isTodayHoliday){
+            price = price.multiply(BigDecimal.valueOf(1.25));
+        }
+        return price;
+    }
+}
